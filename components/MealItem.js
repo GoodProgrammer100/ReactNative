f87321a78f055f704b31ab4d85f5b6d0ba3d7f56
Darshan Poudel <pranishpoudel10@gmail.com>
2023-10-16 15:42:33 +0545

    developed screens

---
+++ components/MealItem.js
@@ -0,0 +1,69 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  Platform,
+} from "react-native";
+
+const MealItem = ({ meal }) => {
+  return (
+    <View style={styles.itemContainer}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <Image style={styles.imageStyle} source={{ uri: meal.imageUrl }} />
+        <View>
+          <Text style={styles.title}>{meal.title}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.infoItem}>{meal.duration}</Text>
+          <Text style={styles.infoItem}>{meal.complexity}</Text>
+          <Text style={styles.infoItem}>{meal.affordability}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default MealItem;
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    height: 320,
+    marginHorizontal: 8,
+    marginVertical: 12,
+    backgroundColor: "white",
+    borderRadius: 20,
+    overflow: "hidden",
+    elevation: 5,
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: Platform.OS === "ios" ? 0.5 : 1,
+  },
+  imageStyle: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  title: {
+    textAlign: "center",
+    marginVertical: 8,
+    fontSize: 18,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingBottom: 8,
+  },
+  infoItem: {
+    marginHorizontal: 6,
+  },
+});
---
+++ components/categoryCard.js
@@ -0,0 +1,53 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+const CategoryCard = ({ children, bgColor, onPress }) => {
+  return (
+    <View style={[styles.cardContainer, { backgroundColor: bgColor }]}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: "#cccccc" }}
+        onPress={onPress.bind(this, children.id)}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.cardText}>{children.title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryCard;
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    width: 150,
+    height: 160,
+    margin: 12,
+    borderRadius: 20,
+    elevation: 8,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+    overflow: "hidden",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  cardText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
